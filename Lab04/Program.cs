0b7cffd964ea0736f614279747267452f3b82c65
Nevyblu3 <10060.phatnari@gmail.com>
2026-09-07 12:54:51 +0700

    add CHARACTER CREATION

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Lab04
+﻿/*
+ * Student ID : 1681401368
+ * Name       : Phatnari Mangthes
+ * Section    : 129A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("===== NEW ADVENTURE =====");
+            //Console.Write("Name your hero:"); // write พิมพ์ต่อ,writeline เว้นบรรทัด
+            //string playerName = Console.ReadLine(); 
+            //Console.WriteLine($"\nWelcome, {playerName}. Your journey begins...");
+
+            //Console.WriteLine("________________________");
+            //Console.WriteLine("Choose Difficulty (1-3)");
+
+            //int difficulty = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($">Difficulty set to :  {difficulty}");
+
+            Console.WriteLine("+________________________+");
+            Console.WriteLine("|   CHARACTER CREATION   |");
+            Console.WriteLine("+________________________+");
+            Console.Write("Name your character:");
+            string characterName = Console.ReadLine();
+            Console.Write("Choose a class (1-3):");
+            bool classValid = int.TryParse(Console.ReadLine(), out int classNumber);
+            Console.Write("Starting luck (0.0 - 10.0):");
+            bool luckValid = double.TryParse(Console.ReadLine(), out double luck);
+            Console.WriteLine($">>{characterName} the Class {classNumber} adventurer enters the dungeon. Luck : {luck}!");
         }
     }
 }
