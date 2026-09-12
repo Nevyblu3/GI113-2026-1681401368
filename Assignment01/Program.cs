d42b20755ad2c94e2880e516d6ac79284fd96ab6
Nevyblu3 <10060.phatnari@gmail.com>
2026-09-12 09:16:03 +0700

    เริ่มต้นกำหนดตัวแปรค่าต่างๆ

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Assignment01
+﻿using System;
+
+namespace Assignment01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const GameTitle = "HonKai: Star Rail";
+
+            var characterName = "Phainon";
+            var characterTier = 'S';
+            int characterHp = 3684;
+            int characterAtk = 2785;
+            int characterDef = 1574;
+            int characteeSpeed = 99;
+            float critDmg = 205.0f;
+            double critRate = 38.7;
+            bool isPlayable = true;
+
+       
+
+            float characterAtkAsFloat = characterAtk;
+
+            int critRateTruncated = (int)critRate;
+            int critRateRounnded = Convert.ToInt32(critRate);
+
+
+
+
+
         }
     }
 }
